4401573f510fd1518c482fd390adb2e1d0754e06
MadeByIToncek <47974277+MadeByIToncek@users.noreply.github.com>
2021-03-09 09:26:12 +0100

    první testovací verze bota

---
+++ features/sender.js
@@ -0,0 +1,7 @@
+module.exports = (client, instance) => {
+  client.on('message', (message) => {
+    if(msg.guild==null &&msg.author.id!=='botDiscordId'){
+    msg.reply('dosomethinghere')
+        
+  })
+}
--- index.js
+++ index.js
@@ -11,23 +11,14 @@ client.on('ready', () => {
   // An empty string = ignored
   const messagesPath = ''
 
-  // Used to configure the database connection.
-  // These are the default options but you can overwrite them
-  const dbOptions = {
-    keepAlive: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  }
-
   // If you want to disable built in commands you can add them to this array. Simply uncomment the strings to disable that command.
 
   const disabledDefaultCommands = [
-    // 'help',
-    // 'command',
-    // 'language',
-    // 'prefix',
-    // 'requiredrole'
+     'help',
+     'command',
+     'language',
+     'prefix',
+     'requiredrole'
   ]
 
   // Initialize WOKCommands with specific folders and MongoDB
@@ -40,9 +31,8 @@ client.on('ready', () => {
     disabledDefaultCommands
   })
     // Set your MongoDB connection path
-    .setMongoPath(process.env.MONGO_URI)
     // Set the default prefix for your bot, it is ! by default
-    .setDefaultPrefix('!')
+    .setDefaultPrefix('/')
     // Set the embed color for your bot. The default help menu will use this. This hex value can be a string too
     .setColor(0xff0000)
 })
