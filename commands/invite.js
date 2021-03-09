ddeaecce02914bde50d1e2caefdfad7ce0e27960
MadeByIToncek <47974277+MadeByIToncek@users.noreply.github.com>
2021-03-09 17:35:46 +0100

    Vyhotoven Embed

--- commands/invite.js
+++ commands/invite.js
@@ -9,14 +9,39 @@ module.exports = {
       message.delete()
       const url = args[0];
       message.reply('Pracuji na tom, chvilku prosím').then(sendMessage())
-
       function sendMessage(message) {
          fetch(url, {
               method: "POST",
               headers: {
                   "Content-Type": "application/json"
               },
-              body: JSON.stringify({"username": "Hashnode Bot", "content": `Test`})
+              body: JSON.stringify({
+                "username": "PlayInRight.cz | Spolupráce",
+                "avatar_url": "https://www.playinright.cz/wp-content/uploads/2020/11/20200801_105227.png",
+                "embeds": [
+                  {
+                    "author": {
+                      "name": "Nimixx#7777",
+                      "icon_url": "https://cdn.discordapp.com/avatars/271212711407255553/8e547c603dded84ac8a5f1ed08cd4aac.png"
+                    },
+                    "title": "Projekt PlayInRight.cz",
+                    "url": "https://playinright.cz/",
+                    "description": "> Náš komunitní projekt je trochu jiný než ostatní \n> Nejdeme cestou jednoho serveru! U nás najde své \n> útočiště každá věková skupina a každý hráč, který \n> není toxický a umí se chovat slušně a dle pravidel. ",
+                    "color": 16744192,
+                    "fields": [
+                      {
+                        "name": "**Co u nás najdete?**",
+                        "value": ":small_orange_diamond: Skvělý a **přátelský A-team**, který Vám vždy pomůže \n:small_orange_diamond: Více Discord serverů s různým zaměřením! \n:small_orange_diamond: **Aktivní projekt**, který je stále inovován a váží si svých členů \n:small_orange_diamond: **Velké Giveaways a eventy!** \n:small_orange_diamond: Vlastní discord boty od hudebního až support \n:small_orange_diamond: Webový portál + Knowledge base \n:small_orange_diamond: **Vlastní Minecraft server pro nejvěrnější** \n:small_orange_diamond: **NonToxic komunita a pohoda každý den!** \n:small_orange_diamond: Bezpečné prostřední pro všechny zajištuje **Wick** bot! \n:small_orange_diamond: Možnosti spolupráce po dohodě! \n:small_orange_diamond: Každý člen má právo se podílet svými nápady na serverech \n:small_orange_diamond: Support prakticky 24/7 díky našemu mail botovi.",
+                        "inline": true
+                      }
+                    ],
+                    "footer": {
+                      "text": "PlayInRight © 2021",
+                      "icon_url": "https://www.playinright.cz/wp-content/uploads/2020/11/20200801_105227.png"
+                    }
+                  }
+                ]
+              })
           });
       }
   }
