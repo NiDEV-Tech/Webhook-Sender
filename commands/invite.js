2ddbe9fc652524b632859f46d90d077332e94c4f
MadeByIToncek <47974277+MadeByIToncek@users.noreply.github.com>
2021-03-09 13:16:13 +0100

    FUNGUJE TOOOOO

---
+++ commands/invite.js
@@ -0,0 +1,23 @@
+// File name: "ping.js"
+// Folder "./commands"
+const fetch = require('node-fetch');
+
+module.exports = {
+    description: 'Pošle naši pozvánku na váš server pomocí webhooku.',
+    category: "server_invite",
+    callback: ({ client, message, args }) => {
+      message.delete()
+      const url = args[0];
+      message.reply('Pracuji na tom, chvilku prosím').then(sendMessage().then(console.log(url)))
+
+      function sendMessage(message) {
+         fetch(url, {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json"
+              },
+              body: JSON.stringify({"username": "Hashnode Bot", "content": `Test`})
+          });
+      }
+  }
+}
--- features/sender.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client, instance) => {
-  client.on('message', (message) => {
-    if(message.guild==null && message.author.id!=="813483295416909854" && message.content=="/server_invite"){
-      message.reply('dosomethinghere')
-  }})
-}
